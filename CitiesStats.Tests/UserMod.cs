1c18b087fa73387bb10f8cb5b316ae1bcaad5d70
Aaron Holmes <aaron@aaronholmes.net>
2019-04-21 02:57:39 -0700

    Add test project.

---
+++ CitiesStats.Tests/UserMod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using ICities;
+using Moq;
+using Xunit;
+
+namespace CitiesStats.Tests
+{
+	public class AutoMoqDataAttribute : AutoDataAttribute
+	{
+		public AutoMoqDataAttribute()
+			: base(() => new Fixture().Customize(new AutoMoqCustomization()))
+		{
+		}
+	}
+
+	public class UserMod
+	{
+		private CitiesStats _modInstance;
+
+		public UserMod()
+		{
+			_modInstance = new CitiesStats();
+		}
+
+		[Theory, AutoMoqData]
+		public void OnSettingsUI_Does_Not_Crash(Mock<UIHelperBase> uiHelperBaseMock)
+		{
+			_modInstance.OnSettingsUI(uiHelperBaseMock.Object);
+		}
+	}
+}
