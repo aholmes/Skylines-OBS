f5222f19beea48d844760a96538f1e8a329a818b
Aaron Holmes <aaron@aaronholmes.net>
2019-04-21 03:07:24 -0700

    Add extra game output for testing.

--- source/Economy/EconomyExtension.cs
+++ source/Economy/EconomyExtension.cs
@@ -12,5 +12,11 @@ namespace CitiesStats.Economy
 			System.IO.File.WriteAllText(@"C:\Users\Aaron\skylinesmod\currentMoneyAmount", (internalMoneyAmount / 100M).ToString("#,#.00#"));
 			return base.OnUpdateMoneyAmount(internalMoneyAmount);
 		}
+
+		public override int OnFetchResource(EconomyResource resource, int amount, Service service, SubService subService, Level level)
+		{
+			System.IO.File.WriteAllText(@"C:\Users\Aaron\skylinesmod\" + resource.ToString(), (amount / 100M).ToString("#,#.00#"));
+			return base.OnFetchResource(resource, amount, service, subService, level);
+		}
 	}
 }
